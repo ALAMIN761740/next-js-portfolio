e4abe01e9d69e3e06333b54330b90c5364a5e236
ALAMIN761740 <abdullahalamin761740@gmail.com>
2026-07-24 00:09:21 +0600

    add logo component with react icon

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+
+import Link from 'next/link'
+import { LuCodeXml } from 'react-icons/lu'
 
 export default function Logo() {
     return (
-        <Link href="/" className="flex items-center">
+        <Link href="/" className="flex items-center gap-3 group">
+            <div className='w-10 h-10 rounded-full 
+            flex items-center 
+            justify-center
+            bg-surface border 
+            border-border 
+            text-primary shadow-sm 
+            group-hover:scale-105
+            group-hover:border-primary
+            transition-all duration-300
+            '>
+                <LuCodeXml className="w-5 h-5" />
+            </div>
         </Link>
     )
 }
